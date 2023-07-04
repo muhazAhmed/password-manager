532c2e44c052ad7f9ac23dcefab13a514a224ea8
FARINE KHAN <farinepkhan520@gmail.com>
2023-07-04 13:13:47 +0530

    pg api

--- server/src/controllers/passwordController.js
+++ server/src/controllers/passwordController.js
@@ -1,38 +1,45 @@
 // const userModel = require('./userModel'); // Import the User model
-const passwordModel = require('../models/passwordModel'); // Import the Password model
-const pass = require('generate-password');
+const passwordModel = require("../models/passwordModel"); // Import the Password model
+const pass = require("generate-password");
 
 const passwordManger = async (req, res) => {
-    try {
-        let data = req.body;
-        let {userId, password} = data
+  try {
+    let data = req.body;
+    let { userId, password } = data;
 
-        const savedData = await passwordModel.create(data)
-        return res.status(201).json({ data: savedData })
-        
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
-}
+    const savedData = await passwordModel.create(data);
+    return res.status(201).json({ data: savedData });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
 
 const getPasswords = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
-}
+  try {
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
 
-const generatePassword = async (req,res)=>{
-    try {
-        var password = pass.generate({
-            length: 8,
-            numbers: true,
-        });
-        res.status(201).json(password)
-    } catch (error) {
-        return res.status(500).json(error.message);
+const generatePassword = async (req, res) => {
+  try {
+    let obj = {length:req.body.length};
+    if (req.body.number == true) {
+      obj.numbers = true;
     }
-}
-module.exports = {passwordManger, getPasswords,generatePassword}
-
+    if (req.body.upper == false) {
+      obj.uppercase = false;
+    }
+    if (req.body.lower == false) {
+      obj.lowercase = false;
+    }
+    if (req.body.symbol == true) {
+      obj.symbols = true;
+    }
+    var password = pass.generate(obj);
+    res.status(201).json(password);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+module.exports = { passwordManger, getPasswords, generatePassword };
