7a45bcad0804d4d26d88e4abdc93bdeafac92a16
Muhaz Ahmed <muhazvla313@gmail.com>
2023-07-05 12:17:12 +0530

    minor fix from navbar and gen page

--- client/src/pages/Password/Generate/PasswordGenerator.jsx
+++ client/src/pages/Password/Generate/PasswordGenerator.jsx
@@ -4,6 +4,8 @@ import { ToggleSwitch } from "react-dragswitch";
 import "react-dragswitch/dist/index.css";
 import usePostAPI from "../../../util/util.services";
 import { ServerVariables } from "../../../util/ServerVariables";
+import Loading from "../../../components/Loading/Loading";
+import svg from "../../../assets/images/Eyes.svg"
 
 const PasswordGenerator = () => {
   const { data, error, loading, postData } = usePostAPI();
@@ -16,68 +18,88 @@ const PasswordGenerator = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const inputs = {
-      length : length,
+      length: length,
       upperCase: upperCase,
       lowerCase: lowerCase,
       numbers: numbers,
-      specialChar: specialChar
+      specialChar: specialChar,
     };
 
     await postData(ServerVariables.Generate, inputs);
   };
 
-  return ( 
+  return (
     <div className="pass-body">
-    <div><h1>Password Generator</h1></div>
-    <div className="out-pass">
-        <h2> Generated Password : {data}</h2>
-    </div>
-      <div className="slider">
-        <span className="min">4</span><input id="slider" name="length" type="range" min="4" max="24" value={length}
-          onChange={(e) => setLength(parseInt(e.target.value))} /><span className="max">24</span> <p>Password Lenght : {length}</p>
+      <div className="pass-header">
+        <div data-aos="slide-down">
+          <h1>Password Generator</h1>
+        </div>
+        <div className="out-pass">
+          <h2> Generated Password</h2> <h3>{data}</h3>
+        </div>
       </div>
-      <div className="toggle-buttons">
-        <label>
-          <span>Include Upper Case</span>
-          <ToggleSwitch
-            checked={upperCase}
-            onChange={(e) => {
-              setUpperCase(e);
-            }}
+      <div className="pass-right" data-aos="zoom-in">
+        <div className="slider" >
+          <div className="pass-range"><span className="min">4</span>
+          <input
+            id="slider"
+            name="length"
+            type="range"
+            min="4"
+            max="24"
+            value={length}
+            onChange={(e) => setLength(parseInt(e.target.value))}
           />
-        </label>
-        <label>
-          <span>Include Lower Case</span>
-          <ToggleSwitch
-            checked={lowerCase}
-            onChange={(e) => {
-              setLowerCase(e);
-            }}
-          />
-        </label>
-        <label>
-          <span>Include Number</span>
-          <ToggleSwitch
-            checked={numbers}
-            onChange={(e) => {
-              setNumbers(e);
-            }}
-          />
-        </label>
-        <label>
-          <span>Include Special Charecters</span>
-          <ToggleSwitch
-            checked={specialChar}
-            onChange={(e) => {
-              setSpecialChar(e);
-            }}
-          />
-        </label>
+          <span className="max">24</span></div>
+          <h4>Password Lenght : {length}</h4>
+        <div>
+        </div>
+        </div>
+        <div className="pass-svg"> <img src={ svg} alt="svg"/></div>
+        <div className="toggle-buttons">
+          <label>
+            <span>Include Upper Case</span>
+            <ToggleSwitch
+              checked={upperCase}
+              onChange={(e) => {
+                setUpperCase(e);
+              }}
+            />
+          </label>
+          <label>
+            <span>Include Lower Case</span>
+            <ToggleSwitch
+              checked={lowerCase}
+              onChange={(e) => {
+                setLowerCase(e);
+              }}
+            />
+          </label>
+          <label>
+            <span>Include Number</span>
+            <ToggleSwitch
+              checked={numbers}
+              onChange={(e) => {
+                setNumbers(e);
+              }}
+            />
+          </label>
+          <label>
+            <span>Include Special Charecters</span>
+            <ToggleSwitch
+              checked={specialChar}
+              onChange={(e) => {
+                setSpecialChar(e);
+              }}
+            />
+          </label>
+        </div>
       </div>
       <div className="gen-btn">
         <button onClick={handleSubmit}>Generate Password</button>
       </div>
-      {error&& <p>{error}</p>}
+      {error && <p>{error}</p>}
+      {loading && <Loading />}
     </div>
   );
 };
