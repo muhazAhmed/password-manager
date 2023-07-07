11078911180f64a61040503e66051d0c1c9979e3
Muhaz Ahmed <muhazvla313@gmail.com>
2023-07-07 11:40:54 +0530

    added clipboard copy

--- client/src/pages/Password/Generate/PasswordGenerator.jsx
+++ client/src/pages/Password/Generate/PasswordGenerator.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./generator.css";
 import { ToggleSwitch } from "react-dragswitch";
 import "react-dragswitch/dist/index.css";
 import usePostAPI from "../../../util/util.services";
 import { ServerVariables } from "../../../util/ServerVariables";
 import Loading from "../../../components/Loading/Loading";
-import svg from "../../../assets/images/Eyes.svg"
+import svg from "../../../assets/images/Eyes.svg";
 
 const PasswordGenerator = () => {
   const { data, error, loading, postData } = usePostAPI();
@@ -14,9 +14,12 @@ const PasswordGenerator = () => {
   const [lowerCase, setLowerCase] = useState(false);
   const [numbers, setNumbers] = useState(false);
   const [specialChar, setSpecialChar] = useState(false);
+  const [openIcon, setOpenIcon] = useState(false);
+  const passwordRef = useRef(null); // Create a ref for the generated password
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setOpenIcon(true);
     const inputs = {
       length: length,
       upperCase: upperCase,
@@ -28,6 +31,16 @@ const PasswordGenerator = () => {
     await postData(ServerVariables.Generate, inputs);
   };
 
+  const handleCopyPassword = () => {
+    const password = passwordRef.current.innerText; // Get the password text
+    const tempInput = document.createElement("input"); // Create a temporary input element
+    tempInput.value = password; // Set its value to the password
+    document.body.appendChild(tempInput); // Append it to the document
+    tempInput.select(); // Select the input's contents
+    document.execCommand("copy"); // Copy the contents to the clipboard
+    document.body.removeChild(tempInput); // Remove the temporary input element
+  };
+
   return (
     <div className="pass-body">
       <div className="pass-header">
@@ -35,27 +48,41 @@ const PasswordGenerator = () => {
           <h1>Password Generator</h1>
         </div>
         <div className="out-pass">
-          <h2> Generated Password</h2> <h3>{data}</h3>
+          <h2> Generated Password</h2>
+          <h3 ref={passwordRef}>
+            {data}{" "}
+            {openIcon && (
+              <i
+                title="copy"
+                className="fa-regular fa-clipboard"
+                onClick={handleCopyPassword}
+              ></i>
+            )}
+          </h3>
         </div>
       </div>
       <div className="pass-right" data-aos="zoom-in">
-        <div className="slider" >
-          <div className="pass-range"><span className="min">4</span>
-          <input
-            id="slider"
-            name="length"
-            type="range"
-            min="4"
-            max="24"
-            value={length}
-            onChange={(e) => setLength(parseInt(e.target.value))}
-          />
-          <span className="max">24</span></div>
+        <div className="slider">
+          <div className="pass-range">
+            <span className="min">4</span>
+            <input
+              id="slider"
+              name="length"
+              type="range"
+              min="4"
+              max="24"
+              value={length}
+              onChange={(e) => setLength(parseInt(e.target.value))}
+            />
+            <span className="max">24</span>
+          </div>
           <h4>Password Lenght : {length}</h4>
-        <div>
+          <div></div>
         </div>
+        <div className="pass-svg">
+          {" "}
+          <img src={svg} alt="svg" />
         </div>
-        <div className="pass-svg"> <img src={ svg} alt="svg"/></div>
         <div className="toggle-buttons">
           <label>
             <span>Include Upper Case</span>
