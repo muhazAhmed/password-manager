dc3d1c590daf0ee94132ae8be5f23b92dd638c1f
Muhaz Ahmed <muhazvla313@gmail.com>
2023-08-02 09:17:14 +0530

    added 404

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
 import "./index.css"
 import Navbar from "./layouts/Navbar/Navbar";
 import Home from './pages/Main/Home/Home'
